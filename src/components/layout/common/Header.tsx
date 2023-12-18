51395109176c4c371da96fefc2a7b37558b65de6
Elias Petry <elias.petry95@gmail.com>
2023-12-18 15:37:59 +0000

    Refactor routes, add text logo art to header and footer, adapt header responsiveness and add links to footer

--- src/components/layout/common/Header.tsx
+++ src/components/layout/common/Header.tsx
@@ -1,28 +1,28 @@
 import { Link } from 'react-router-dom';
 import luffyFlag from '../../../../public/img/luffy-flag.png';
+import textLogo from '../../../../public/img/text-logo.png';
+import { routes, Route } from './routes';
 import * as styles from '../../../styles/layout/header.module.sass';
 
 const Header: React.FC = () => (
     <div id={styles.header}>
         <nav>
             <ul>
-                <li>
-                    <Link to="/">Inicio</Link>
-                </li>
-                <li>
-                    <Link to="/about">Presentación</Link>
-                </li>
-                <li>
-                    <Link to="/categories">Categorías</Link>
-                </li>
-                <li>
-                    <Link to="/credits">Enlaces</Link>
-                </li>
+                {routes.map(({ name, route }: Route) => (
+                    <li>
+                        <Link to={route}>{name}</Link>
+                    </li>
+                ))}
             </ul>
         </nav>
         <div id={styles['logo-wrapper']}>
             <Link to="/">
                 <img id={styles.logo} src={luffyFlag} alt="logo" />
+                <img
+                    id={styles['text-logo']}
+                    src={textLogo}
+                    alt="logotipo de texto"
+                />
             </Link>
         </div>
     </div>
---
+++ src/components/layout/common/routes.ts
@@ -0,0 +1,32 @@
+import categoryDescriptions from '../../categories/categoryDescriptions';
+
+export interface Route {
+    name: string;
+    route: string;
+}
+
+const routes: Route[] = [
+    {
+        name: 'Inicio',
+        route: '/'
+    },
+    {
+        name: 'Presentación',
+        route: '/about'
+    },
+    {
+        name: 'Categorías',
+        route: '/categories'
+    },
+    {
+        name: 'Enlaces',
+        route: '/credits'
+    }
+];
+
+const categoryRoutes: Route[] = categoryDescriptions.map(category => ({
+    name: category.title,
+    route: category.route
+}));
+
+export { routes, categoryRoutes };
